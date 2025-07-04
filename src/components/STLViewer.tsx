f670a53ee6a460fbe5c52e473a1694c1af8bef08
Johnny Prat <info@id3dconcept.com>
2025-07-04 12:00:24 +0200

    fix: correct import Mesh/BufferGeometry

--- src/components/STLViewer.tsx
+++ src/components/STLViewer.tsx
@@ -3,13 +3,15 @@ import { useEffect, useRef, useState } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import { STLLoader } from 'three-stdlib';
-import * as THREE from 'three';
+import { Mesh, BufferGeometry } from 'three';
+
 
 
 
 function Model({ url }: { url: string }) {
-  const ref = useRef<THREE.Mesh>(null);
-  const [geometry, setGeometry] = useState<THREE.BufferGeometry | null>(null);
+const ref = useRef<Mesh>(null);
+const [geometry, setGeometry] = useState<BufferGeometry | null>(null);
+
 
   useEffect(() => {
     // Charger le STL côté client

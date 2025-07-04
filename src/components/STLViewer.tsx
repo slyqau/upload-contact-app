73ad66dd3bf0bf9a3730233b87519cbc24dea42f
slyqau <jalo650@gmail.com>
2025-07-04 11:56:24 +0200

    Update STLViewer.tsx

--- src/components/STLViewer.tsx
+++ src/components/STLViewer.tsx
@@ -3,7 +3,8 @@ import { useEffect, useRef, useState } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import { STLLoader } from 'three-stdlib';
-import * as THREE from 'three';
+import { Mesh, BufferGeometry } from 'three';
+
 
 
 function Model({ url }: { url: string }) {

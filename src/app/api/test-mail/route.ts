3db67b63e3d1ccfbd1ac85807679f6d4b46cb1c2
slyqau <jalo650@gmail.com>
2025-07-07 11:58:16 +0200

    Update route.ts

--- src/app/api/test-mail/route.ts
+++ src/app/api/test-mail/route.ts
@@ -1,9 +1,9 @@
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function GET() {
+  const resend = new Resend(process.env.RESEND_API_KEY);
   try {
     const mailResult = await resend.emails.send({
       from: 'onboarding@resend.dev',
